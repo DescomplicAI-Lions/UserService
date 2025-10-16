c94d8c033eb5a5e9f3d39713fa123968f7257990
Felipe Martins <106782887+AstraSama@users.noreply.github.com>
2025-10-16 20:24:13 -0300

    Update database.ts
    para testar o vercel

--- src/database/database.ts
+++ src/database/database.ts
@@ -3,48 +3,48 @@ import { config } from "../config/env";
 import { Database as DatabaseResult } from "../interfaces/database.interface";
 
 class Database {
-    private pool: Pool;
+    // private pool: Pool;
 
-    constructor() {
-        const poolConfig: PoolConfig = {
-            host: config.db_host,
-            port: Number(config.db_port),
-            database: config.db_database,
-            user: config.db_user,
-            password: config.db_password,
-            ssl: {
-                rejectUnauthorized: false
-            },
-            max: 20,
-            idleTimeoutMillis: 30000
-        };
+    // constructor() {
+    //     const poolConfig: PoolConfig = {
+    //         host: config.db_host,
+    //         port: Number(config.db_port),
+    //         database: config.db_database,
+    //         user: config.db_user,
+    //         password: config.db_password,
+    //         ssl: {
+    //             rejectUnauthorized: false
+    //         },
+    //         max: 20,
+    //         idleTimeoutMillis: 30000
+    //     };
 
-        this.pool = new Pool(poolConfig);
-    }
+    //     this.pool = new Pool(poolConfig);
+    // }
 
-    async query(sql: string, values?: any[]): Promise<DatabaseResult> {
-        try {
-            const result: QueryResult = await this.pool.query(sql, values);
+    // async query(sql: string, values?: any[]): Promise<DatabaseResult> {
+    //     try {
+    //         const result: QueryResult = await this.pool.query(sql, values);
 
-            return {
-                status: true,
-                data: result.rows
-            }
-        } catch( error ) {
-            return {
-                status: false,
-                data: [],
-                error: error as Error
-            }
-        }
+    //         return {
+    //             status: true,
+    //             data: result.rows
+    //         }
+    //     } catch( error ) {
+    //         return {
+    //             status: false,
+    //             data: [],
+    //             error: error as Error
+    //         }
+    //     }
 
-    }
+    // }
 
-    async end(): Promise<void> {
-        await this.pool.end();
-        console.log("Conexão com bancode ddados encerrada!");
+    // async end(): Promise<void> {
+    //     await this.pool.end();
+    //     console.log("Conexão com bancode ddados encerrada!");
         
-    }
+    // }
 }
 
 const database = new Database();
