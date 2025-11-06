cb1a6750d23b8129a4653bc8373eb4a1ae6a4869
AstraSama <felippemart777@gmail.com>
2025-11-06 20:01:40 -0300

    alterados arquivos app e server para conexão com o vercel

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,22 @@
-import { app } from "./app";
-import { config } from "./config/env"
+import app from "./app";
+import { database } from "./database/database";
+import { config } from "./config/env";
 
-app.listen(config.app_port, () => {
-    console.log(`Servidor ${config.app_name} iniciado na porta ${config.app_port}`);    
-})
+const PORT = Number(config.app_port) || 3000;
+
+const startServer = async () => {
+  try {
+    const dbTest = await database.query('SELECT NOW()');
+    if (dbTest.status) {
+      console.log('Database connection successful!');
+    }
+  } catch (err) {
+    console.error('Error during database startup test:', err);
+  }
+
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+};
+
+startServer();
