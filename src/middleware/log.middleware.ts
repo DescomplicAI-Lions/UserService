0b1ba70a88f2422b06c5aac5c707426b8545d754
AstraSama <felippemart777@gmail.com>
2025-11-10 20:03:59 -0300

    Mudança de versão dotenv e alteração do log.middleware.ts para uma versão console.log para utilizar no vercel

--- src/middleware/log.middleware.ts
+++ src/middleware/log.middleware.ts
@@ -1,19 +1,23 @@
 import morgan from "morgan";
-import fs from "fs";
-import path from "path";
+// import fs from "fs"; // REMOVIDO PARA O VERCEL
+// import path from "path"; // REMOVIDO PARA O VERCEL
 import { Request, Response } from "express";
 
-const storageDir = path.join(__dirname, "..", "storage");
-if (!fs.existsSync(storageDir)) {
-    fs.mkdirSync(storageDir, { recursive: true });
-}
-
-const filelog = fs.createWriteStream(path.join(storageDir, "acess.log"), { flags: "a" });
+// REMOVIDO PARA O VERCEL
+// const storageDir = path.join(__dirname, "..", "storage");
+// if (!fs.existsSync(storageDir)) {
+//     fs.mkdirSync(storageDir, { recursive: true });
+// }
+// const filelog = fs.createWriteStream(path.join(storageDir, "acess.log"), { flags: "a" });
 
 morgan.token('cid', function  (req: Request, res: Response) {
-  return 'no-cid';
+  return req.headers['x-request-id'] as string || 'no-cid';
 });
 
 const customLogFormat = ":remote-addr - :remote-user [:date[clf]] \":method :url HTTP/:http-version\" :status :res[content-length] \":referrer\" \":user-agent\" cid=:cid";
 
-export const log = morgan(customLogFormat, { stream: filelog });
+export const log = morgan(customLogFormat, {
+  stream: {
+    write: (message: string) => console.log(message.trim()),
+  },
+});
