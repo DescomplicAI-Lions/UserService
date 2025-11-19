09094789122487d588f04b103e882d025e939369
AstraSama <felippemart777@gmail.com>
2025-11-19 20:38:17 -0300

    feat: Atualizado o arquivo de documentação swagger

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,10 @@
 import { Router } from "express";
-import { exampleRoutes } from "./example.routes";
 import { userRoutes } from "./user.routes";
 import { magicLinkRoutes } from "./magicLink.routes";
 import { emailConfirmationRoutes } from "./emailConfirmatio.routes"; 
 
 const routes: Router = Router();
 
-routes.use(exampleRoutes);
 routes.use("/users", userRoutes);
 routes.use('/auth', magicLinkRoutes);
 routes.use('/email-confirmation', emailConfirmationRoutes); 
